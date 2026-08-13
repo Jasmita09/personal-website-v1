0ae6fb482f6139aa1c701ea9cb1a71329e8a30a8
Jasmita <jasmita.iragam09@gmail.com>
2026-08-13 19:22:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -153,3 +153,6 @@ document.addEventListener("DOMContentLoaded", function() {
     restartBtn.addEventListener("click", restartQuiz);
   }
 });
+function showEmailPopup() {
+  alert("Contact Jasmita R. Iragam:\n\nEmail: jasmita.iragam09@gmail.com");
+}
